d42307f80872bbd2f8d505b58e7ae01f53face6b
NaphatMqa <naphat@mapquestasia.com>
2024-04-26 14:57:57 +0700

    Creat marker where user click on map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,39 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { MapContainer, TileLayer, useMap, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, GeoJSON, Marker, useMapEvents } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
-import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic'
+//import NoSSR from 'react-no-ssr';
+import { Icon, LatLngBoundsExpression, LatLngExpression, bounds, map } from "leaflet";
+import { ReactNode, useEffect, useLayoutEffect, useState } from 'react'
+import { saveAs } from 'file-saver';
 
-export default function Home() {
+const DefaultOnSSR: React.FC = () => null
+
+export const NoSSR: React.FC<{ children: ReactNode; onSSR?: ReactNode }> = ({ children, onSSR = <DefaultOnSSR /> }) => {
+  const [onBrowser, setOnBrowser] = useState(false)
+  useLayoutEffect(() => {
+    setOnBrowser(true)
+  }, [])
+  return <>{onBrowser ? children : onSSR}</>
+}
+
+export default function Home(this: any) {
+  const center: LatLngExpression = [0, 0];
+  const zoom = 5;
+  const bounds: LatLngBoundsExpression | undefined = [[-90, -180], [90, 180]];
+
+  const markerIcon = new Icon(
+    {
+      iconUrl: ("./pin.png"),
+      iconSize: [30, 30]
+    }
+  );
 
   const [geoFile, setGeoFile] = useState<File>();
   const [geoContent, setGeoContent] = useState<any>(null);
+  const [markers, setMarkers] = useState<[number, number][]>([]);
 
   const handleGeoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -26,28 +50,61 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    if (geoContent != null) {
-      console.log(typeof (geoContent));
-      console.log(geoContent);
-    }
-  }, [geoContent])
+  const LocationFinder = () => {
+    const map = useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        setMarkers([...markers, [lat, lng]]);
+      },
+    });
+    return null;
+  };
 
-  const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
-    ssr: false
-  });
-  const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
-    ssr: false
-  });
-  const GeoJSON = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), {
-    ssr: false
-  });
-  
+  const exportFile = () => {
+    const features = markers.map((position) => ({
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates: position
+      }
+    }));
+
+    const geojsonData = {
+      type: "FeatureCollection",
+      features: features
+    };
+    const blob = new Blob([JSON.stringify(geojsonData)], { type: "application/json" });
+    saveAs(blob, "markers.geojson");
+  };
+
+  useEffect(() => {
+    //console.log(markers);
+    //console.log(geojsonData);
+  }, [markers])
+
+  console.log(geoContent);
+  /*
+      const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
+        ssr: false
+      });
+    
+      const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
+        ssr: false
+      });
+      
+      const GeoJSON = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), {
+        ssr: false
+      });
+      
+      const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), {
+        ssr: false
+      });
+    */
 
   return (
     <>
-
-      <div className='flex border-2 border-black box-content h-10vw w-56 p-4 center'>
+      <div className='flex space-x-3 border-2 border-blue-300 m-4 rounded-lg box-content h-10vw w-fit p-4 center'>
         <label>
           <input
             type="file"
@@ -59,21 +116,75 @@ export default function Home() {
             <span>Geojson</span>
           </div>
         </label>
-      </div>
 
-      <MapContainer center={[13.75, 100.7]} zoom={10.5} scrollWheelZoom={true}>
+        {/*geoContent != null ? (
+          <button
+            className="HeaderButton flex items-center justify-center border-2 cursor-pointer"
+            onClick={() => setGeoContent(null)}>
+            <span>Reset polygon</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="DisableButton flex items-center justify-center" disabled
+          >
+            Reset polygon
+          </button>
+        )*/}
 
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {geoContent != null && (
-          < GeoJSON data={geoContent} />
+        {markers.length > 0 ? (
+          <button 
+          className="HeaderButton flex items-center justify-center border-2 cursor-pointer"
+          onClick={exportFile}>
+            <span>Export</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="DisableButton flex items-center justify-center" disabled>
+            Export
+          </button>
         )}
 
-      </MapContainer>
+        {markers.length > 0 ? (
+          <button
+            className="HeaderButton p-3 flex items-center justify-center border-2 cursor-pointer"
+            onClick={() => setMarkers([])}>
+            <span>Clear Mark</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="DisableButton p-3 flex items-center justify-center" disabled>
+            <span>{"Clear Mark"}</span>
+          </button>
+        )}
+      </div>
+      <NoSSR>
+        <MapContainer
+          center={center}
+          zoom={2.5}
+          scrollWheelZoom={true}
+          maxBounds={bounds}
+          maxBoundsViscosity={1.0}
+          bounceAtZoomLimits={true}
+          minZoom={2.5}
+        >
 
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          <LocationFinder />
+          {markers.map((position, index) => (
+            <Marker key={index} position={position} icon={markerIcon} />
+          ))}
+
+          {geoContent != null ? (
+            < GeoJSON data={geoContent} />
+          ) : null}
+        </MapContainer >
+      </NoSSR>
     </>
   );
 }
-
-
