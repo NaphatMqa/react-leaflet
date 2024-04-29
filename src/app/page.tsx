6c124a44e5662795028c9c8934e2b7b36f5caac8
NaphatMqa <naphat@mapquestasia.com>
2024-04-29 10:15:22 +0700

    Fix point not display, Fix GeoJSON data not rendered after change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,169 +1,162 @@
 'use client'
 
-  import React from "react";
-  import ReactDOM from "react-dom";
-  import { MapContainer, TileLayer, useMap, GeoJSON, Marker, useMapEvents } from 'react-leaflet'
-  import "leaflet/dist/leaflet.css"
-  import dynamic from 'next/dynamic'
-  //import NoSSR from 'react-no-ssr';
-  import { Icon, LatLng, LatLngBoundsExpression, LatLngExpression, bounds, map } from "leaflet";
-  import { useState } from 'react'
-  import { saveAs } from 'file-saver';
+import React, { LegacyRef, useEffect, useRef } from "react";
+import { MapContainer, TileLayer, GeoJSON, Marker, useMapEvents } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+import L, { Icon, LatLngBoundsExpression, LatLngExpression } from "leaflet";
+import { useState } from 'react'
+import { saveAs } from 'file-saver';
+import { GeoJSON as LeafletGeoJSON } from "leaflet";
 
-  /*
-  const DefaultOnSSR: React.FC = () => null
-  
-  export const NoSSR: React.FC<{ children: ReactNode; onSSR?: ReactNode }> = ({ children, onSSR = <DefaultOnSSR /> }) => {
-    const [onBrowser, setOnBrowser] = useState(false)
-    useLayoutEffect(() => {
-      setOnBrowser(true)
-    }, [])
-    return <>{onBrowser ? children : onSSR}</>
+export default function Home(this: any) {
+
+  const center: LatLngExpression = [0, 0];
+  const bounds: LatLngBoundsExpression | undefined = [[-90, -180], [90, 180]];
+  const [geoFile, setGeoFile] = useState<File>();
+  const [geoContent, setGeoContent] = useState<any>(null);
+  const [markers, setMarkers] = useState<[number, number][]>([]);
+  const [fileNum, setFileNum] = useState<number>(1);
+  const geoJsonRef = useRef<any>()
+
+
+  useEffect(() => {
+    if (geoJsonRef.current) {
+      geoJsonRef.current.clearLayers()   
+      geoJsonRef.current.addData(geoContent) 
+    }
+  }, [geoJsonRef, geoContent])
+
+  const markerIcon = new Icon(
+    {
+      iconUrl: ("./pin.png"),
+      iconSize: [30, 30]
+    }
+  );
+
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        setMarkers([...markers, [lat, lng]]);
+      },
+    });
+    return null;
+  };
+
+  L.Marker.prototype.options.icon = L.icon({
+    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
+  });
+
+  function onEachFeature(feature: any, layer: L.Layer) {
+    if (feature.properties) {
+      const { popupContent } = feature.properties;
+      layer.bindPopup(popupContent);
+    }
   }
-  */
-  export default function Home(this: any) {
 
-    const center: LatLngExpression = [0, 0];
-    const bounds: LatLngBoundsExpression | undefined = [[-90, -180], [90, 180]];
-    const [geoFile, setGeoFile] = useState<File>();
-    const [geoContent, setGeoContent] = useState<any>(null);
-    const [markers, setMarkers] = useState<[number, number][]>([]);
+  const handleGeoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      setGeoFile(file);
 
-    const markerIcon = new Icon(
-      {
-        iconUrl: ("./pin.png"),
-        iconSize: [30, 30]
-      }
-    );
-
-    const handleGeoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const file = event.target.files?.[0];
-      if (file) {
-        const reader = new FileReader();
-        setGeoFile(file);
-
-        reader.onload = (e) => {
-          const content = e.target?.result as string;
-          setGeoContent(JSON.parse(content));
-        };
-        reader.readAsText(file);
-      }
-    };
-  
-    const LocationMarker = () => {
-      const map = useMapEvents({
-        click(e) {
-          const { lat, lng } = e.latlng;
-          setMarkers([...markers, [lat, lng]]);
-        },
-      });
-      return null;
-    };
-
-    const exportFile = () => {
-      const features = markers.map((position) => ({
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "Point",
-          coordinates: [position[1], position[0]]
-        }
-      }));
-
-      const geojsonData = {
-        type: "FeatureCollection",
-        features: features
+      reader.onload = (e) => {
+        const content = e.target?.result as string;
+        setGeoContent(JSON.parse(content));
       };
-      const blob = new Blob([JSON.stringify(geojsonData)], { type: "application/json" });
-      saveAs(blob, "markers.geojson");
+      reader.readAsText(file);
+    }
+  };
+
+  const exportFile = () => {
+    const features = markers.map((position) => ({
+      type: "Feature",
+      properties: {
+        "popupContent": `lng: ${position[0]} lat: ${position[1]}`
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [position[1], position[0]]
+      }
+    }));
+
+    const geojsonData = {
+      type: "FeatureCollection",
+      features: features
     };
+    const blob = new Blob([JSON.stringify(geojsonData)], { type: "application/json" });
+    saveAs(blob, `marker${fileNum}.geojson`);
+    setFileNum(fileNum + 1);
+  };
 
-    /*
-        const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
-          ssr: false
-        });
-      
-        const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
-          ssr: false
-        });
-        
-        const GeoJSON = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), {
-          ssr: false
-        });
-        
-        const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), {
-          ssr: false
-        });
-      */
+  return (
+    <>
+      <div className='flex space-x-3 border-2 border-blue-300 m-4 rounded-lg box-content h-10vw w-fit p-4 center'>
+        <label>
+          <input
+            type="file"
+            accept=".geojson"
+            hidden
+            onChange={handleGeoChange}
+          />
+          <div className="HeaderButton flex items-center justify-center border-2 cursor-pointer">
+            <span>Geojson</span>
+          </div>
+        </label>
 
-    return (
-      <>
-        <div className='flex space-x-3 border-2 border-blue-300 m-4 rounded-lg box-content h-10vw w-fit p-4 center'>
-          <label>
-            <input
-              type="file"
-              accept=".geojson"
-              hidden
-              onChange={handleGeoChange}
-            />
-            <div className="HeaderButton flex items-center justify-center border-2 cursor-pointer">
-              <span>Geojson</span>
-            </div>
-          </label>
+        {markers.length > 0 ? (
+          <button
+            className="HeaderButton flex items-center justify-center border-2 cursor-pointer"
+            onClick={exportFile}>
+            <span>Export</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="DisableButton flex items-center justify-center" disabled>
+            Export
+          </button>
+        )}
 
-          {markers.length > 0 ? (
-            <button
-              className="HeaderButton flex items-center justify-center border-2 cursor-pointer"
-              onClick={exportFile}>
-              <span>Export</span>
-            </button>
-          ) : (
-            <button
-              type="button"
-              className="DisableButton flex items-center justify-center" disabled>
-              Export
-            </button>
-          )}
+        {markers.length > 0 ? (
+          <button
+            className="HeaderButton p-3 flex items-center justify-center border-2 cursor-pointer"
+            onClick={() => setMarkers([])}>
+            <span>Clear</span>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="DisableButton p-3 flex items-center justify-center" disabled>
+            <span>{"Clear"}</span>
+          </button>
+        )}
+      </div>
 
-          {markers.length > 0 ? (
-            <button
-              className="HeaderButton p-3 flex items-center justify-center border-2 cursor-pointer"
-              onClick={() => setMarkers([])}>
-              <span>Clear</span>
-            </button>
-          ) : (
-            <button
-              type="button"
-              className="DisableButton p-3 flex items-center justify-center" disabled>
-              <span>{"Clear"}</span>
-            </button>
-          )}
-        </div>
+      <MapContainer
+        center={center}
+        zoom={2.5}
+        scrollWheelZoom={true}
+        maxBounds={bounds}
+        maxBoundsViscosity={1.0}
+        bounceAtZoomLimits={true}
+        minZoom={2.5}
+      >
 
-        <MapContainer
-          center={center}
-          zoom={2.5}
-          scrollWheelZoom={true}
-          maxBounds={bounds}
-          maxBoundsViscosity={1.0}
-          bounceAtZoomLimits={true}
-          minZoom={2.5}
-        >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationMarker />
+        {markers.map((position, index) => (
+          <Marker key={index} position={position} icon={markerIcon} />
+        ))}
 
-          <LocationMarker/>
-          {markers.map((position, index) => (
-            <Marker key={index} position={position} icon={markerIcon} />
-          ))}
+        {geoContent != null ? (
+          < GeoJSON data={geoContent} onEachFeature={onEachFeature} ref={geoJsonRef}/>
+        ) : null}
+      </MapContainer >
 
-          {geoContent != null ? (
-            < GeoJSON data={geoContent}/>
-          ) : null}
-        </MapContainer >
-
-      </>
-    );
-  }
+    </>
+  );
+}
